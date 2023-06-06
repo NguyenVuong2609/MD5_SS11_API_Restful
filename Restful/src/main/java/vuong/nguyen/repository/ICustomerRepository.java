a054977b08e87e9f57ba9aaf578f168fc9904b33
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2023-06-06 20:10:28 +0700

    restful

---
+++ Restful/src/main/java/vuong/nguyen/repository/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package vuong.nguyen.repository;
+
+import vuong.nguyen.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
